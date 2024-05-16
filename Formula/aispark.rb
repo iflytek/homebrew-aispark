42d4e766245f357e375f3a7cd98b456e1bc0185b
smallKeyboy <marjorjian@gmail.com>
2024-05-16 17:35:06 +0800

    feat:first init

--- Formula/aispark.rb
+++ Formula/aispark.rb
@@ -4,27 +4,23 @@ class Aispark < Formula
   version "0.1.3"
 
   if Hardware::CPU.arm?
-    url "https://github.com/iflytek/spark-ai-cli/releases/download/v0.1.3/aispark-darwin-arm64.gz"
-    sha256 "9996ECE44904EF9DBAB31BBC04E8427A679212B42DB78A8A67CEDCC2277A050D"
+    url "https://github.com/iflytek/spark-ai-cli/releases/download/v0.1.3/aispark-darwin-arm64.tar.gz"
+    sha256 "76D10D4E4B74CFF0D252B883A5328CD976990FE29AF6160A960AB8814A590B86"
   else
-    url "https://github.com/iflytek/spark-ai-cli/releases/download/v0.1.3/aispark-darwin-amd64.gz"
-    sha256 "69B8227CAE1397E14A0A5D00530F54C024A91F7EEB00F65D0C8E088AD01796C9"
+    url "https://github.com/iflytek/spark-ai-cli/releases/download/v0.1.3/aispark-darwin-amd64.tar.gz"
+    sha256 "E981320BFF80046BF32EB42B5AE61D93640B19F603F518D2E69DC26C9D6DE90C"
   end
 
   def install
+    # 解压 .tar.gz 文件
+    system "tar", "-xzf", cached_download
+
+    # 将解压后的可执行文件安装到 bin 目录
     if Hardware::CPU.arm?
-      gz_file = "aispark-darwin-arm64.gz"
-      binary_file = "aispark-darwin-arm64"
+      bin.install "aispark-darwin-arm64" => "aispark"
     else
-      gz_file = "aispark-darwin-amd64.gz"
-      binary_file = "aispark-darwin-amd64"
+      bin.install "aispark-darwin-amd64" => "aispark"
     end
-
-    # 解压 .gz 文件
-    system "gzip", "-d", buildpath/gz_file
-
-    # 安装解压后的文件
-    bin.install buildpath/binary_file => "aispark"
   end
 
   test do
