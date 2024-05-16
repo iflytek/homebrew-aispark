2007328e71f204b5e22ac347fbfe55f55190191d
smallKeyboy <marjorjian@gmail.com>
2024-05-16 21:51:49 +0800

    init

--- Formula/aispark.rb
+++ Formula/aispark.rb
@@ -3,8 +3,8 @@ class Aispark < Formula
   homepage "https://github.com/iflytek/spark-ai-cli"
   version "v0.1.4"
 
-  armsha256 "5cd85fde259bdf0b41148e7e52ab907af8077e687e671f7084e74961e2d62419"
-  amdsha256 "04c2d537a8bf4defb7831a1be6fddda0f44f345ea6454e16464d9f4dddfb7221"
+  armsha256="5cd85fde259bdf0b41148e7e52ab907af8077e687e671f7084e74961e2d62419"
+  amdsha256="04c2d537a8bf4defb7831a1be6fddda0f44f345ea6454e16464d9f4dddfb7221"
   if Hardware::CPU.arm?
     url "http://repo.model.xfyun.cn/api/packages/aispark/generic/aispark/v0.1.4/aispark-darwin-arm64.tar.gz"
     sha256 armsha256
