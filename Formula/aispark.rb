496def6887f6b43962eeba6759eacee3332a6c85
iflytek-opensource <aicloud@iflytek.com>
2024-05-16 14:16:38 +0000

    update version to v0.1.4

--- Formula/aispark.rb
+++ Formula/aispark.rb
@@ -3,8 +3,8 @@ class Aispark < Formula
   homepage "https://github.com/iflytek/spark-ai-cli"
   version "v0.1.4"
 
-  armsha256="5cd85fde259bdf0b41148e7e52ab907af8077e687e671f7084e74961e2d62419"
-  amdsha256="04c2d537a8bf4defb7831a1be6fddda0f44f345ea6454e16464d9f4dddfb7221"
+  armsha256="9ccc7cc644770304a4cd23bbb1b97f4c0231418c72b3f06b5a3c366350b892b9"
+  amdsha256="ab959820320078a83d9cf6cbd5863bd4f06b406d8ee7f4d9b5f216ed2417cf22"
   if Hardware::CPU.arm?
     url "http://repo.model.xfyun.cn/api/packages/aispark/generic/aispark/v0.1.4/aispark-darwin-arm64.tar.gz"
     sha256 armsha256
